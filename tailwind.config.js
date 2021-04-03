cb85285ca7cb76561455ff3bbac1bc4f9b03b371
Sam King <sdking02@gmail.com>
2021-04-03 11:51:12 +0100

    Use tailwind forms to improve appearance

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ['./*.html', './css/*', './main.js'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        teal: colors.teal
+      }
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
